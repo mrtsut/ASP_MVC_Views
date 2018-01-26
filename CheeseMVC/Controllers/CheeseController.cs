d79fcc326c0e4fd165e9466ed9f0e362caa3d36c
Trevor Sutcliffe <mrtsut@hotmail.com>
2018-01-26 15:17:15 -0600

    Changes up to 27 min in tutorial video

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private List<string> Cheeses = new List<string>();   // static - available to any code in the class
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            
+            ViewBag.cheeses = Cheeses;   //Object to pass between controller and view
+
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult NewCheese(string name)
+        {
+            // Add the new chese to my existing cheeses
+            Cheeses.Add(name);
+
+
+            return Redirect("/Cheese");
+
+        }
+    }
+}
